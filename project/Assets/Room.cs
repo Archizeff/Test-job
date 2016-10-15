40752d9734fbe99fe7331d0ebff4d81cc882a57e
mihail-ПК\mihail <althunin@gmail.com>
2016-10-15 11:12:03 +0300

    Add Region calculatio

---
+++ project/Assets/Room.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Room : MonoBehaviour {
+
+    public float width, height;
+    private float cordX, cordY;
+    public GameObject room;
+
+    private void CreateRoom(int[] room)
+    {
+    }
+
+    void Start()
+    {
+        GameObject obj = Instantiate(room);
+        obj.transform.position = new Vector2(cordX, cordY);
+        obj.transform.localScale = new Vector2(width, height);
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ project/Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public static float unit = 0.2f;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
